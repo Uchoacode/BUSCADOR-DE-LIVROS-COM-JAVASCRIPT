f08f11e6c5892858157b4bff5249c3a3f5c41149
Rian Uchôa <158238927+Uchoacode@users.noreply.github.com>
2024-06-05 17:11:28 -0300

    script.js

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
+document.getElementById('search').addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+        e.preventDefault(); // Impede o comportamento padrão de submissão do formulário
+        searchBooks();
+    }
+});
+
 function searchBooks() {
-  const query = document.getElementById('search').value;
-  const url = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
+    const query = document.getElementById('search').value;
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
 
-  fetch(url)
-  .then(response => response.json())
-  .then(data => {
-      const resultsDiv = document.getElementById('results');
-      resultsDiv.innerHTML = '';
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const resultsDiv = document.getElementById('results');
+            resultsDiv.innerHTML = '';
 
-      data.items.forEach(item => {
-          const title = item.volumeInfo.title;
-          const authors = item.volumeInfo.authors ? item.volumeInfo.authors.join(', ') : 'Unknown';
-          const thumbnail = item.volumeInfo.imageLinks.thumbnail ? item.volumeInfo.imageLinks.thumbnail : 'https://via.placeholder.com/150';
+            if (data.items) {
+                data.items.forEach(item => {
+                    const title = item.volumeInfo.title;
+                    const authors = item.volumeInfo.authors ? item.volumeInfo.authors.join(', ') : 'Unknown';
 
-          const bookDiv = document.createElement('div');
-          bookDiv.classList.add('book');
-          bookDiv.innerHTML = `
-              <h3>${title}</h3>
-              <p style="font-family: Verdana, Geneva, Tahoma, sans-serif;">Author(s): ${authors}</p>
-              <img src="${thumbnail}" alt="${title}">
-          `;
-          resultsDiv.appendChild(bookDiv);
-      });
-  })
-  .catch(error => console.error('Error:', error));
+                    const bookDiv = document.createElement('div');
+                    bookDiv.classList.add('book');
+                    bookDiv.innerHTML = `
+                        <h3>${title}</h3>
+                        <p>Author(s): ${authors}</p>
+                    `;
+                    resultsDiv.appendChild(bookDiv);
+                });
+            } else {
+                resultsDiv.innerHTML = '<p>Nenhum resultado encontrado.</p>';
+            }
+        })
+        .catch(error => console.error('Erro:', error));
 }
